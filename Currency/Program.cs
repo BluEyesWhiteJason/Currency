d4bc82569c41f5a23d43d102b9d894af4e46f202
BluEyesWhiteJason <jason.speet@gmail.com>
2019-06-09 12:43:49 -0400

    Shoring up some formating, final version for real

--- Currency/Program.cs
+++ Currency/Program.cs
@@ -21,27 +21,27 @@ namespace Currency
             double total = firstAmt + secondAmt + thirdAmt;
             double avg = total / 3;
 
-            Console.WriteLine("The average of the entered values is $" + Math.Round(avg, 2));
+            Console.WriteLine("");
+            Console.WriteLine("The average of the entered values is " + String.Format("{0:c}", avg));
 
             // Place values in array, sort, say highest and lowest
             double[] values = { firstAmt, secondAmt, thirdAmt };
             Array.Sort(values);
 
-            Console.WriteLine("The highest value entered is $" + Math.Round(values[2], 2));
+            Console.WriteLine("The highest value entered is " + String.Format("{0:c}", values[2]));
 
-            Console.WriteLine("The lowest value entered is $" + Math.Round(values[0], 2));
+            Console.WriteLine("The lowest value entered is " + String.Format("{0:c}", values[0]));
 
             // Format total as different currencies
             // Method for us format found here: https://www.howtogeek.com/howto/programming/format-a-string-as-currency-in-c/
             // Method for other formats found here: https://stackoverflow.com/questions/35499585/string-format-for-japanese-currency
-            // Method for thai found here
             string usa = String.Format("US: {0:c}", total);
             string sw = total.ToString("C", new CultureInfo("sv-SE"));
             string jp = total.ToString("C", new CultureInfo("ja-JP"));
             string thai = total.ToString("#,##0.00");
 
-
-
+            Console.WriteLine("");
+            Console.WriteLine("Total with international formats:");
             Console.WriteLine(usa);
             Console.WriteLine("Sweden: " + sw);
             Console.WriteLine("Japan: " + jp);
